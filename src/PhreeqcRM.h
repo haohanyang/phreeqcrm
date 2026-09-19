bfeddd81eec29813ba77545bd1c42356d884b114
Haohan Yang <yanghaohanmail@gmail.com>
2026-09-19 12:20:06 +0200

    feat: use openmp dynamic scheduler

--- src/PhreeqcRM.h
+++ src/PhreeqcRM.h
@@ -5597,10 +5597,14 @@ private:
 	void                                      GatherNchem(std::vector< double > &source, std::vector< double > &destination);
 	cxxStorageBin &                           Get_phreeqc_bin(void) {return *this->phreeqc_bin;}
 	IRM_RESULT                                HandleErrorsInternal(std::vector< int > & r);
+	IRM_RESULT                                HandleErrorsInternal2(std::vector< std::pair<int, int> > & r);
 	void                                      PartitionUZ(int n, int iphrq, int ihst, double new_frac);
 	void                                      RebalanceLoad(void);
 	void                                      RebalanceLoadPerCell(void);
 	IRM_RESULT                                RunCellsThread(int i);
+	IRM_RESULT                                RunCell(int n, int i);
+	void                                      BeforeRunCellsThread(int n);
+	void                                      AfterRunCellsThread(int n);
 	IRM_RESULT                                RunFileThread(int n);
 	IRM_RESULT                                RunStringThread(int n, std::string & input);
 	IRM_RESULT                                RunCellsThreadNoPrint(int n);
